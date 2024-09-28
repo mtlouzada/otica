05b3f601d35555fb699081ac47f3ba8f5a5e6ed6
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-09-27 23:56:16 -0300

    init tailwind

--- src/pages/components/Topo-Banner/index.jsx
+++ src/pages/components/Topo-Banner/index.jsx
@@ -4,7 +4,7 @@ import styles from "./banner.module.css";
 export default function Banner() {
     return(
         <section className={styles.ContainerBanner}>
-            <div>
+            <div className={styles.texto}>
                 <p>Preços baixos em</p>
                 <h1>Óculos de Grau e de sol</h1>
                 <p>você só encontra aqui</p>
